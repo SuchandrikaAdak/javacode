095fe9f7f14d27099667659ddee28730e3cf4898
Suchandrika <suchandrika21121997@gmail.com>
2021-10-30 13:09:35 +0530

    First commit

---
+++ frontend/tut53.js
@@ -0,0 +1,22 @@
+console.log("this is tutorial");
+
+
+// default value of greettext if no value paased
+function greet(name,greettext="this is agreettext")
+{
+    console.log(greettext+" "+ name);
+    console.log(name+" is a good bpoy");
+}
+let greettext="good morning";
+let name="harry";
+greet(name,greettext);
+
+function sum(a ,b,c)
+{
+    let d=a+b+c;
+    return d;
+}
+
+let sumans=sum(1,2,3);
+console.log(sumans);
+
